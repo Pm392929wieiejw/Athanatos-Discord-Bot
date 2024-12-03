4715f05fe5b9e5ef02b2257d1440529c96077045
Pm392929wieiejw <edward.julca@cimas.edu.pe>
2024-12-03 23:35:37 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,15 +1,36 @@
 package main
 
+// imports
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
+
+//starts http server
+func main() {
+	go func() {
+		http.HandleFunc("/", getRoot)
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Athanatos is at render.com now.. 🚀\n")
+}
 
 func main() {
 	godotenv.Load()
